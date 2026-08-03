bd61f80dc949433644bb0a7c1c88ab03069c17dd
mdakashhossain1 <hossainakash22958@gmail.com>
2026-08-03 15:17:03 +0530

    chore: add .gitignore to exclude node_modules, temp, downloads, and environment files

--- push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo # Youtube-Video-Downloader >> README.md
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/mdakashhossain1/Youtube-Video-Downloader.git
-git push -u origin main
-pause
